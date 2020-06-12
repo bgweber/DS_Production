a72dcec6b99459815f64b4ef91590258ac46e9a8
Raman Jafarau <drf95@mail.ru>
2020-06-12 13:01:02 +0000

    Complete section 2.3 Model Endpoints

--- source/scripts/keras_games.py
+++ source/scripts/keras_games.py
@@ -1,21 +1,12 @@
-
-import pandas as pd
+import flask
 import mlflow
 import mlflow.keras
-import flask
-import tensorflow as tf
-import keras as k
+import pandas as pd
+import tensorflow.keras as K
 
-def auc(y_true, y_pred):
-    auc = tf.metrics.auc(y_true, y_pred)[1]
-    k.backend.get_session().run(
-                  tf.local_variables_initializer())
-    return auc
 
-global graph
-graph = tf.get_default_graph()
 model_path = "models/keras_games_v1"
-model = mlflow.keras.load_model(model_path, custom_objects={'auc': auc})
+model = mlflow.keras.load_model(model_path)
 
 app = flask.Flask(__name__)
 
@@ -25,23 +16,20 @@ def predict():
     params = flask.request.args
 
     if "G1" in params.keys(): 
-        new_row = { "G1": params.get("G1"), "G2": params.get("G2"), 
-                    "G3": params.get("G3"), "G4": params.get("G4"), 
-                    "G5": params.get("G5"), "G6": params.get("G6"), 
-                    "G7": params.get("G7"), "G8": params.get("G8"), 
-                    "G9": params.get("G9"), "G10": params.get("G10") }
-
-        new_x = pd.DataFrame.from_dict(new_row, orient = "index").transpose()                
-
-        with graph.as_default():        
-            data["response"] = str(model.predict(new_x)[0][0])
-            data["success"] = True
+        new_row = {"G1": params.get("G1"), "G2": params.get("G2"), 
+                   "G3": params.get("G3"), "G4": params.get("G4"), 
+                   "G5": params.get("G5"), "G6": params.get("G6"), 
+                   "G7": params.get("G7"), "G8": params.get("G8"), 
+                   "G9": params.get("G9"), "G10": params.get("G10")}
+        
+        new_x = pd.DataFrame.from_dict(data=new_row,   
+                                       orient="index", 
+                                       dtype='int').T
+     
+        data["response"] = str(model.predict(new_x)[0][0])
+        data["success"] = True
 
     return flask.jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
-#model.evaluate(x, y, verbose = 0)
-
-
--- source/scripts/predict.py
+++ source/scripts/predict.py
@@ -1,16 +1,17 @@
-
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-#import pickle
+# import pickle
+import flask
 import mlflow
 import mlflow.sklearn
+import pandas as pd
+
+#from sklearn.linear_model import LogisticRegression
 
 
-#model = pickle.load(open("logit.pkl", 'rb'))
+# model = pickle.load(open("logit.pkl", 'rb'))
 model_path = "models/logit_games_v1"
 model  = mlflow.sklearn.load_model(model_path)
 
-import flask
+
 app = flask.Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
@@ -19,13 +20,16 @@ def predict():
     params = flask.request.args
 
     if "G1" in params.keys(): 
-        new_row = { "G1": params.get("G1"), "G2": params.get("G2"), 
-                    "G3": params.get("G3"), "G4": params.get("G4"), 
-                    "G5": params.get("G5"), "G6": params.get("G6"), 
-                    "G7": params.get("G7"), "G8": params.get("G8"), 
-                    "G9": params.get("G9"), "G10":params.get("G10") }
+        new_row = {"G1": params.get("G1"), "G2": params.get("G2"), 
+                   "G3": params.get("G3"), "G4": params.get("G4"), 
+                   "G5": params.get("G5"), "G6": params.get("G6"), 
+                   "G7": params.get("G7"), "G8": params.get("G8"), 
+                   "G9": params.get("G9"), "G10":params.get("G10")}
 
-        new_x = pd.DataFrame.from_dict(new_row, orient = "index").transpose()                
+        new_x = pd.DataFrame.from_dict(data=new_row, 
+                                       orient="index", 
+                                       dtype='int').T
+        
         data["response"] = str(model.predict_proba(new_x)[0][1])
         data["success"] = True
 
@@ -33,4 +37,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-

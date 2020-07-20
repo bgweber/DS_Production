f46867ae108d07fa5b6f43854ffc99e8489a2e83
Raman Jafarau <drf95@mail.ru>
2020-07-20 22:55:20 +0000

    Continue section 3.3 Lambda Functions (AWS)

---
+++ source/scripts/aws_lambda_function/logit/logit.py
@@ -0,0 +1,30 @@
+import json
+import joblib
+import pandas as pd
+
+
+model = joblib.load('logit.pkl')
+
+
+def lambda_handler(event, context):
+    # read in the request body as the event dict
+    if "body" in event:
+        event = event["body"]
+        event = json.loads(event) if event else {}
+    
+    if "G1" in event:
+        new_row = {"G1": event["G1"], "G2": event["G2"],
+                   "G3": event["G3"], "G4": event["G4"],
+                   "G5": event["G5"], "G6": event["G6"], 
+                   "G7": event["G7"], "G8": event["G8"], 
+                   "G9": event["G9"], "G10": event["G10"]}
+        
+        new_x = pd.DataFrame.from_dict(data=new_row, 
+                                       orient="index",
+                                       dtype="float").T
+        
+        prediction = str(model.predict_proba(new_x)[0][1])
+        
+        return {"body": "Prediction " + prediction}
+    
+    return {"body": "No parameters"}

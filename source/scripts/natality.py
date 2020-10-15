e4535cee611837707644776064cf95eb798f03cc
Raman Jafarau <drf95@mail.ru>
2020-10-15 23:03:31 +0000

    Complete section 8.2 Dataflow Streaming

--- source/scripts/natality.py
+++ source/scripts/natality.py
@@ -1,74 +1,65 @@
-import apache_beam as beam
-import argparse
-from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.io.gcp.bigquery import parse_table_schema_from_json
+
 import json
+import argparse
+import joblib
+import pandas as pd
+import apache_beam as beam
+
+from google.cloud import storage
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import GoogleCloudOptions
+from apache_beam.io.gcp.datastore.v1new.types import Key
+from apache_beam.io.gcp.datastore.v1new.types import Entity
+from apache_beam.io.gcp.datastore.v1new.datastoreio import WriteToDatastore
 
 class ApplyDoFn(beam.DoFn):
 
     def __init__(self):
-        self._model = None
-        from google.cloud import storage
-        import pandas as pd
-        import pickle as pkl
-        import json as js
-        self._storage = storage
-        self._pkl = pkl
-        self._pd = pd
-        self._json = js
+        self.model = None
      
     def process(self, element):
-        if self._model is None:
-            bucket = self._storage.Client().get_bucket(
-                                                 'dsp_model_store')
+        if self.model is None:
+            bucket = storage.Client().get_bucket('dsp_model_store_00')
             blob = bucket.get_blob('natality/sklearn-linear')
-            self._model =self._pkl.loads(blob.download_as_string())
+            blob.download_to_filename('sklearn-linear')
+            self.model = joblib.load('sklearn-linear')
         
-        element = self._json.loads(element.decode('utf-8'))
-        new_x = self._pd.DataFrame.from_dict(element, 
-                            orient = "index").transpose().fillna(0)   
-        weight = self._model.predict(new_x.iloc[:,1:8])[0]
-        print(str(weight))
-        return [ { 'guid': element['guid'], 'weight': weight, 
-                                   'time': str(element['time']) } ]
-             
-class PublishDoFn(beam.DoFn):
-    
-    def __init__(self):
-        from google.cloud import datastore       
-        self._ds = datastore
-    
+        element = json.loads(element.decode('utf-8'))
+        new_x = pd.DataFrame.from_dict(element, orient="index").T.fillna(0)   
+        weight = self.model.predict(new_x.iloc[:, :8])[0]
+        yield {'guid': element['guid'],
+               'weight': weight,
+               'time': str(element['time'])}
+        
+class CreateEntityDoFn(beam.DoFn):
     def process(self, element):
-        client = self._ds.Client()
-        key = client.key('natality-guid', element['guid'])
-        entity = self._ds.Entity(key)
-        entity['weight'] = element['weight']         
-        entity['time'] = element['time']
-        print("publish")
-        print(entity)
-        client.put(entity)
-            
-# set up pipeline parameters 
+        key = Key(['natality-guid', element['guid']])
+        entity = Entity(key)
+        entity.set_properties({
+            'weight': element['weight'],
+            'time': element['time']
+        })
+        yield entity
+        
+# set up pipeline options
 parser = argparse.ArgumentParser()
-known_args, pipeline_args = parser.parse_known_args(None)
+known_args, pipeline_args = parser.parse_known_args()
 pipeline_options = PipelineOptions(pipeline_args)
+project = pipeline_options.view_as(GoogleCloudOptions).project
 
 # define the topics 
 topic = "projects/{project}/topics/{topic}"
-topic = topic.format(project = "gameanalytics-199018", topic = "natality")
+topic = topic.format(project=project, topic="natality")
 
-schema = parse_table_schema_from_json(json.dumps({'fields':
-            [ { 'name': 'guid', 'type': 'STRING'},
-              { 'name': 'weight', 'type': 'FLOAT64'},
-              { 'name': 'time', 'type': 'STRING'} ]}))
-
-# define the pipeline steps 
+# define the pipeline steps
 p = beam.Pipeline(options=pipeline_options)
-lines = p | 'Read PubSub' >> beam.io.ReadFromPubSub(topic=topic)
-scored = lines | 'apply' >> beam.ParDo(ApplyDoFn())
-scored | 'Create entities' >> beam.ParDo(PublishDoFn())
+(p
+ | 'Read PubSub' >> beam.io.ReadFromPubSub(topic=topic)
+ | 'Apply Model' >> beam.ParDo(ApplyDoFn())
+ | 'Create Entities' >> beam.ParDo(CreateEntityDoFn())
+ | 'Save to Datastore' >> WriteToDatastore(project)
+)
 
-# run the pipeline 
+# run the pipeline
 result = p.run()
 result.wait_until_finish()
-
--- source/scripts/stream.py
+++ source/scripts/stream.py
@@ -1,26 +1,30 @@
-import apache_beam as beam
+
 import argparse
+import apache_beam as beam
+
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import GoogleCloudOptions
 
 # define a function for transforming the data 
 class AppendDoFn(beam.DoFn):
     def process(self, element):
-        print("Hellow World! - " + element)
+        print("Hello World! - " + element.decode('utf-8'))
         
-# set up pipeline parameters 
+# set up pipeline options
 parser = argparse.ArgumentParser()
-known_args, pipeline_args = parser.parse_known_args(None)
+known_args, pipeline_args = parser.parse_known_args()
 pipeline_options = PipelineOptions(pipeline_args)
+project = pipeline_options.view_as(GoogleCloudOptions).project
 
 # define the topics 
 topic = "projects/{project}/topics/{topic}"
-topic = topic.format(project = "gameanalytics-199018", topic = "natality")
-
+topic = topic.format(project=project, topic="natality")
 
 # define the pipeline steps 
 p = beam.Pipeline(options=pipeline_options)
-lines = p | 'Read PubSub' >> beam.io.ReadFromPubSub(topic=topic)
-appended = lines | 'append' >> beam.ParDo(AppendDoFn())
+(p
+ | 'Read PubSub' >> beam.io.ReadFromPubSub(topic=topic)
+ | 'Append' >> beam.ParDo(AppendDoFn()))
 
 # run the pipeline 
 result = p.run()
